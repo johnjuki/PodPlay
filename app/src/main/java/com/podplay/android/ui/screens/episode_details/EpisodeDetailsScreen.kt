8251d782f51cdd178b97e6c49b9117f0185cbb5e
John Njuki <njukijohn2000@gmail.com>
2023-04-29 10:20:40 +0300

    add dummy data

--- app/src/main/java/com/podplay/android/ui/screens/episode_details/EpisodeDetailsScreen.kt
+++ app/src/main/java/com/podplay/android/ui/screens/episode_details/EpisodeDetailsScreen.kt
@@ -10,15 +10,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.podplay.android.R
+import com.podplay.android.model.PodcastDummyData
 import com.podplay.android.util.DateUtils
 import com.podplay.android.util.HtmlUtils
 
@@ -88,27 +92,54 @@ fun EpisodeDetailsScreen(
                             .placeholder(R.drawable.logo)
                             .crossfade(true)
                             .build(),
+                        contentScale = ContentScale.Crop,
                         error = painterResource(id = R.drawable.logo),
                         contentDescription = null,
+                        modifier = Modifier
+                            .height(120.dp)
+                            .clip(MaterialTheme.shapes.medium)
                     )
+
                     Spacer(modifier = Modifier.height(20.dp))
-                    Text(text = episode.title)
+
+                    Text(text = episode.title, style = MaterialTheme.typography.titleLarge)
+
                     Spacer(modifier = Modifier.height(20.dp))
-                    Text(text = uiState.podcastName)
+
+                    Text(text = uiState.podcastName, style = MaterialTheme.typography.bodyMedium)
+
                     Spacer(modifier = Modifier.height(5.dp))
+
                     Row {
                         Text(text = DateUtils.dateToShortDate(episode.releaseDate))
                         Text(text = " - ")
                         Text(text = episode.duration)
                     }
+
                     Spacer(modifier = Modifier.height(20.dp))
-                    Button(onClick = { /*TODO*/ }) {
-                        Text(text = stringResource(R.string.play))
+
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.width(200.dp)) {
+                        Text(text = stringResource(R.string.play), style = MaterialTheme.typography.labelLarge)
                     }
+
                     Spacer(modifier = Modifier.height(20.dp))
+                    
                     Text(text = HtmlUtils.htmlToSpannable(episode.description).toString())
                 }
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EpisodeDetailsScreenPreview() {
+    EpisodeDetailsScreen(
+        navigateUp = { },
+        uiState = EpisodeDetailsUiState(
+            episode = PodcastDummyData.podcast.episodes.first(),
+            podcastName = PodcastDummyData.podcast.feedTitle,
+            podcastImageUrl = PodcastDummyData.podcast.imageUrl
+        )
+    )
 }
